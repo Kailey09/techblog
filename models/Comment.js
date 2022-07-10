056aa563501d9204c9214280457528f4600a149e
Kailey09 <kcoop1323@gmail.com>
2022-07-09 22:26:48 -0400

    added route to create comment

--- models/Comment.js
+++ models/Comment.js
@@ -16,3 +16,20 @@ router.get("/", (req, res) => {
     });
 });
 
+// create a comment
+router.post('/', (req, res) => {
+    if(req.sessionn) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id
+        })
+        .then(dbCommentData => res.json (dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
+});
+
+module.exports = router;
