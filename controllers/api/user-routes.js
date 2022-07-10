2a7377776320c3749007abb7c142acbf25111e7f
Kailey09 <kcoop1323@gmail.com>
2022-07-09 23:04:05 -0400

    added route to get one user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -19,3 +19,36 @@ router.get('/', (req,res) => {
     });
 });
 
+// get specific user
+router.get("/:id", (req, res) => {
+    User.findOne({
+        attributes: {
+            exclude: ['password']
+        },
+        include: [{
+            model: Post,
+            attributes: ['id', 'title', 'content', 'created_at']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'created_at'],
+            include: {
+                model: Post,
+                attributes: ['title']
+            }
+        }
+    ]
+    })
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(400).json({
+                message: 'No user found with this id'
+            });
+            return;
+        }
+    })
+    .cath(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
